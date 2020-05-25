5004e09a971229de53486d0daccf517a2722b63e
Sam Clulow <sam.g.clulow@gmail.com>
2020-05-25 22:28:16 +0200

    initial commit

---
+++ src/controllers/linksController.ts
@@ -0,0 +1,68 @@
+import { Request, Response, NextFunction } from "express";
+import { Link, ILink, LinkType } from '../model/link'
+import * as shortid from 'shortid'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const baseUrl = process.env.BASE_URL
+
+export const createLink = async ( req: Request, res: Response ): Promise<void> => {
+  try {
+    const gid: string = shortid.generate()
+    const originalLink = req.body.originalLink
+    const generatedLink: string = baseUrl ? `https://${baseUrl}/${gid}` : `https://${req.headers.host}/${gid}`
+    const linkObject: ILink = {
+      'originalLink': originalLink,
+      'generatedLink': generatedLink,
+      'GID': gid,
+      'popularity': 0
+    }
+    const newLink: LinkType = new Link( linkObject )
+    await Link.findOneAndUpdate( { originalLink: originalLink }, newLink )
+    res.status( 201 ).json( newLink );
+  } catch ( error ) {
+    res.status( 404 ).json( { error: `${error}` } )
+  }
+}
+
+
+const links = async (): Promise<ILink[]> => await Link.find( {} )
+
+
+export const getAllLinks = async ( req: Request, res: Response, next: NextFunction ): Promise<void> => {
+  const l = await links()
+  res.json( { title: 'All Links', ...l } )
+  next()
+};
+
+
+export const topThree = async ( req: Request, res: Response, next: NextFunction ): Promise<void> => {
+  const link = await Link.find( { popularity: { $gte: 1 } } )
+    .sort( { popularity: -1 } )
+    .limit( 3 );
+  res.json( { title: "most popular links", links: link } )
+};
+
+
+export const getLinkById = async ( req: Request, res: Response, next: NextFunction, ): Promise<void> => {
+  const gid = req.params.gid
+  try {
+    // increment popularity of link
+    const url = await Link.findOneAndUpdate( { GID: gid }, { $inc: { popularity: 1 } } )
+    url == null ? res.status( 301 ).redirect( "/api" ) : res.redirect( 301, `https://${url.originalLink}` )
+  } catch ( error ) {
+    res.status( 301 ).redirect( "/api" )
+  }
+  next()
+}
+
+
+export const deleteLink = async ( req: Request, res: Response ): Promise<void> => {
+  const gid = req.params.gid
+  try {
+    await Link.findOneAndDelete( { GID: gid } )
+    res.status( 204 )
+  } catch ( error ) {
+    res.status( 404 ).json( { error: `${error}` } )
+  }
+}
